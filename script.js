7832b91eb24849a8a5ce7d03335a9fa88ea121ad
MaciejDurda <maciej.durda.1@gmail.com>
2020-01-16 17:33:11 +0100

    Music App

---
+++ script.js
@@ -0,0 +1,28 @@
+window.addEventListener('load',()=>{
+    const sounds = document.querySelectorAll('.sound');
+    const pads = document.querySelectorAll('.pads div');
+    const visual = document.querySelector('.visual');
+    const colors = ['#60d394','#d36060','#c060d3','#d3d160','#2d676b','#3570a0']
+
+    console.log(sounds);
+
+    //Lets geet going with the sound here
+    pads.forEach((pad, index) => {
+        pad.addEventListener('click',function(){
+            sounds[index].currentTime = 0
+            sounds[index].play()
+            createBubbles(index)
+        })
+    })
+
+    //Create a function that makes bubbles
+    const createBubbles = (index)=> {
+        const bubble = document.createElement('div');
+        visual.appendChild(bubble);
+        bubble.style.backgroundColor = colors[index]
+        bubble.style.animation = 'jump 1s ease';
+        bubble.addEventListener('animationend',function(){
+            visual.removeChild(this)
+        })
+    }
+})
